8624df8065cd8545a67715200edb247fa1982b63
Himanshu <htiwarih@gmail.com>
2018-12-12 17:30:18 +0530

    Using function for UI components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
-import Contributor from './Contributor';
+import Contributors from './Contributors';
 
 class App extends Component {
+    state = {
+        contributors : [
+            { name : 'Himanshu Tiwari', username : 'himanshu-tiwari', access : 'write', id : 1 },
+            { name : 'Aditya Chawla', username : 'chawlaaditya8', access : 'read', id : 2 },
+            { name : 'Himanshu Tiwari', username : 'htiwarih0', access : 'read', id : 3 }
+        ]
+    };
+    
     render() {
         return (
             <div className="App">
                 <h1>Sample React Application</h1>
                 <p>Contributors:-</p>
 
-                <Contributor name='Himanshu Tiwari' username='himanshu-tiwari' access='write' />
-                <Contributor name='Aditya Chawla' username='chawlaaditya8' access='read' />
+                <Contributors contributors={ this.state.contributors } />
             </div>
         );
     }
---
+++ src/Contributors.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Contributors = ({ contributors }) => {
+	const contributorList = contributors.map(contributor => {
+		return (
+			<div className="contributor" key={ contributor.id }>
+				<div>Name: { contributor.name }</div>
+				<div>Username: { contributor.username }</div>
+				<div>Access Level: { contributor.access }</div>
+			</div>
+		);
+	});
+
+	return(
+		<div className="contributor-list">
+			{ contributorList }
+		</div>
+	);
+}
+
+export default Contributors;
