6e67d74e30eb26225f4cfe59ef9f19e2f01c11a9
Himanshu Tiwari <himanshutiwari@Himanshus-MacBook-Air.local>
2018-12-12 14:52:48 +0530

    Added a nested component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Contributor from './Contributor';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
+                <h1>Sample React Application</h1>
+                <p>Contributors:-</p>
+                <Contributor />
             </div>
         );
     }
---
+++ src/Contributor.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Contributor extends Component {
+	render() {
+		return(
+			<div className="contributors">
+				<div>Name: Himanshu</div>
+				<div>Age: 23</div>
+				<div>Access Level: Write</div>
+			</div>
+		);
+	}
+}
+
+export default Contributor;
